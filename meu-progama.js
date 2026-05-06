7f7bc3c08393acc194865385a8ffacf0de01a5dc
Samuel Nunes da Silva <samuel62622766@edu.df.senac.br>
2026-05-06 17:39:33 -0300

    java scrpit

---
+++ meu-progama.js
@@ -0,0 +1,7 @@
+let nome= window.prompt("Digiter seu nome: ")
+
+let anoNascimento= window.prompt("Digite o ano do seu ano Nascimento: ")
+
+let idade=  2026-anoNascimento
+
+window.alert("seu nome é "+ nome + " e voce tem "+ idade + "anos")
---
+++ script.js
@@ -0,0 +1,14 @@
+window.alert("OLÁ mundo")
+
+window.confirm("voce quer aprender")
+
+window.prompt("qual é seu nome?")
+
+var nome= "maria"
+let idadedapessoa= 20 
+let temaulasexta= false
+let temaulaamanha= true
+let peso= 54.5 
+let turmasenactecnicoinformatica= '454785489'
+
+
